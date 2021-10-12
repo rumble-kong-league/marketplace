9243e2f2d81aaf276959a40f5da7a1851a0cb323
nazariyv <nazariy@inbox.ru>
2021-10-12 16:23:36 +0100

    license comment

--- contracts/interfaces/IMarketplace.sol
+++ contracts/interfaces/IMarketplace.sol
@@ -83,37 +83,37 @@ interface IMarketplace {
 }
 
 /*
-
-88888888ba  88      a8P  88         
-88      "8b 88    ,88'   88         
-88      ,8P 88  ,88"     88         
-88aaaaaa8P' 88,d88'      88         
-88""""88'   8888"88,     88         
-88    `8b   88P   Y8b    88         
-88     `8b  88     "88,  88         
-88      `8b 88       Y8b 88888888888
-
-* Marketplace: interfaces/IMarketplace.sol
-*
-* MIT License
-* ===========
-*
-* Copyright (c) 2022 Marketplace
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-*/
+ *
+ * 88888888ba  88      a8P  88
+ * 88      "8b 88    ,88'   88
+ * 88      ,8P 88  ,88"     88
+ * 88aaaaaa8P' 88,d88'      88
+ * 88""""88'   8888"88,     88
+ * 88    `8b   88P   Y8b    88
+ * 88     `8b  88     "88,  88
+ * 88      `8b 88       Y8b 88888888888
+ *
+ * Marketplace: interfaces/IMarketplace.sol
+ *
+ * MIT License
+ * ===========
+ *
+ * Copyright (c) 2022 Marketplace
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ */
