95194af1c396d119ad59cc0fef3d695be29a367a
Naz <nazariyv@users.noreply.github.com>
2021-10-12 18:53:15 +0100

    corrected license

--- contracts/interfaces/NFTContract.sol
+++ contracts/interfaces/NFTContract.sol
@@ -27,7 +27,7 @@ interface NFTContract {
  * MIT License
  * ===========
  *
- * Copyright (c) 2022 Marketplace
+ * Copyright (c) 2022 Rumble League Studios Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
